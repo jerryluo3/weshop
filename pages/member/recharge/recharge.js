523c5c479342dce762545ca235808345b964625a
ilofxyt123 <455171468@qq.com>
2018-08-01 12:30:32 +0800

    更新：     pages / member / member           1.暂时关闭"基本设置"功能      pages / shelf / index.js           1.添加物品时如果库存不足，wx.showtoast进行提示           2.修复一个bug：当添加的物品排列在《全部》分类标签中大于第十个时，二次进入便利架页面，会出现无法加载数据（原因：先加载10个，之前缓存的购物车数据，如果商品超过10个，将无法读取到10个以后的数据，无法对对应的物品进行设置之前用户操作时的数量）

--- pages/member/recharge/recharge.js
+++ pages/member/recharge/recharge.js
@@ -57,7 +57,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // this.getAccountMoneyList();
+    this.getAccountMoneyList();
     var uid = wx.getStorageSync("uid");
     this.getUserInfo(uid)
   },
